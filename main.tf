027d189d8090143cc5d3a35d053a8e7c5b361fba
Radek Kubiak <raadek@gmail.com>
2021-10-03 18:45:21 +0200

    state db fix

--- main.tf
+++ main.tf
@@ -164,15 +164,6 @@ resource "aws_security_group_rule" "allow_all_outbound" {
   cidr_blocks = local.all_ips
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-west-1"
-  }
-}
-
 locals {
   http_port    = 80
   any_port     = 0
